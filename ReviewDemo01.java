eff7555c593798085acb131e57ebdc1b270a576f
tiger5720 <tiger5720@naver.com>
2021-09-11 18:27:22 +0900

    version 02

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -17,3 +17,26 @@ public class ReviewDemo01 {
             System.out.println(number+"은(는) 소수가 아닙니다!");
     }
 }
+
+/*import java.util.Scanner;
+// v0.2
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int cnt = 0;
+
+        for(int k=2; k<number; k++) //반복 횟수 2회 감소
+        {
+            if(number % k ==0)
+                cnt++;
+
+        }
+        if(cnt == 0) //cnt가 0이 유지되면 true
+            System.out.println(number+"은(는) 소수!");
+        else
+            System.out.println(number+"은(는) 소수가 아닙니다!");
+
+    }
+}*/
